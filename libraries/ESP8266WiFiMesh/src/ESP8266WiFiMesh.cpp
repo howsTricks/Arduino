d55604775c916edd8f5a2b1561791dc41af33312
chennings-T420 <hennings@gmail.com>
2016-09-25 13:07:20 +0200

    testing initial

--- libraries/ESP8266WiFiMesh/src/ESP8266WiFiMesh.cpp
+++ libraries/ESP8266WiFiMesh/src/ESP8266WiFiMesh.cpp
@@ -2,9 +2,9 @@
   ESP8266WiFiMesh.cpp - Mesh network node
   Sets up a Mesh Node which acts as a router, creating a Mesh Network with other nodes. All information
   is passed in both directions, but it is up to the user what the data sent is and how it is dealt with.
- 
+
   Copyright (c) 2015 Julian Fell. All rights reserved.
- 
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -20,13 +20,14 @@
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
+#include <WiFiClient.h>
 #include <WiFiServer.h>
 
 #include "ESP8266WiFiMesh.h"
 
 #define SSID_PREFIX      		"Mesh_Node"
 #define SERVER_IP_ADDR			"192.168.4.1"
+#define TEST
 #define SERVER_PORT				4011
 
 ESP8266WiFiMesh::ESP8266WiFiMesh(uint32_t chip_id, std::function<String(String)> handler)
@@ -49,7 +50,7 @@ void ESP8266WiFiMesh::begin()
  * Wait for a WiFiClient to connect
  *
  * @returns: True if the client is ready, false otherwise.
- * 
+ *
  */
 bool ESP8266WiFiMesh::waitForClient(WiFiClient curr_client, int max_wait)
 {
@@ -60,7 +61,7 @@ bool ESP8266WiFiMesh::waitForClient(WiFiClient curr_client, int max_wait)
 	/* Return false if the client isn't ready to communicate */
 	if (WiFi.status() == WL_DISCONNECTED || !curr_client.connected())
 		return false;
-	
+
 	return true;
 }
 
@@ -71,7 +72,7 @@ bool ESP8266WiFiMesh::waitForClient(WiFiClient curr_client, int max_wait)
  * @target_ssid The name of the AP the other node has set up.
  * @message The string to send to the node.
  * @returns: True if the exchange was a succes, false otherwise.
- * 
+ *
  */
 bool ESP8266WiFiMesh::exchangeInfo(String message, WiFiClient curr_client)
 {
@@ -83,7 +84,7 @@ bool ESP8266WiFiMesh::exchangeInfo(String message, WiFiClient curr_client)
 	String response = curr_client.readStringUntil('\r');
 	curr_client.readStringUntil('\n');
 
-	if (response.length() <= 2) 
+	if (response.length() <= 2)
 		return false;
 
 	/* Pass data to user callback */
@@ -96,7 +97,7 @@ bool ESP8266WiFiMesh::exchangeInfo(String message, WiFiClient curr_client)
  *
  * @target_ssid The name of the AP the other node has set up.
  * @message The string to send to the node.
- * 
+ *
  */
 void ESP8266WiFiMesh::connectToNode(String target_ssid, String message)
 {
@@ -112,7 +113,7 @@ void ESP8266WiFiMesh::connectToNode(String target_ssid, String message)
 		return;
 
 	/* Connect to the node's server */
-	if (!curr_client.connect(SERVER_IP_ADDR, SERVER_PORT)) 
+	if (!curr_client.connect(SERVER_IP_ADDR, SERVER_PORT))
 		return;
 
 	if (!exchangeInfo(message, curr_client))
